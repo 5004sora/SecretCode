d5b7e052431ab1675fc1e771ba024348190b8547
Elijah Gutierrez <s1740779@ed.ac.uk>
2020-08-21 18:59:17 +0100

    Updates

---
+++ precache-manifest.eca11801f01a52c18993293df3588d2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1216e651b13a2ebee52e5ce22bee14",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "4800c1ed4126e294f3e0",
+    "url": "/app/static/css/main.4fee0fca.chunk.css"
+  },
+  {
+    "revision": "5b06cd54d35999c63de9",
+    "url": "/app/static/js/2.649d60d1.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/app/static/js/2.649d60d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4800c1ed4126e294f3e0",
+    "url": "/app/static/js/main.fb9d4cdf.chunk.js"
+  },
+  {
+    "revision": "44812d8db49eb7d77e58",
+    "url": "/app/static/js/runtime-main.af6f1b9e.js"
+  },
+  {
+    "revision": "f0f3a878e29f6a0587445c8dea84772e",
+    "url": "/app/static/media/ProgSocLogo.f0f3a878.png"
+  }
+]);
